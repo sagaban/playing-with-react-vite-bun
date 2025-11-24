f48a734a4cb1759e63b3621f11f0fc597047872b
Santiago Bandiera <sagaban@hotmail.com>
2025-11-24 18:43:56 +0100

    Refactor OnScrollComponent and OnScrollOnParent to include height styling for better layout control

--- src/components/OnScrollComponent/OnScrollComponent.tsx
+++ src/components/OnScrollComponent/OnScrollComponent.tsx
@@ -14,10 +14,10 @@ const ComponentWithScroll = ({ children }: { children: JSX.Element }): JSX.Eleme
 export const OnScrollComponent = (): JSX.Element => {
   return (
     <ComponentWithScroll>
-      <>
+      <Box sx={{ mt: 1, height: '300px' }}>
         <Box>I am the parent</Box>
         <GlowingOnRenderBox sx={{ height: '150px' }}>I am an expensive component</GlowingOnRenderBox>
-      </>
+      </Box>
     </ComponentWithScroll>
   );
 };
--- src/components/OnScrollComponent/OnScrollOnParent.tsx
+++ src/components/OnScrollComponent/OnScrollOnParent.tsx
@@ -6,7 +6,7 @@ export const OnScrollOnParent = (): JSX.Element => {
   const [, setScroll] = useState({});
   return (
     <GlowingOnRenderBox onScroll={e => setScroll(e)} sx={{ height: '200px', overflowY: 'scroll' }}>
-      <Box sx={{ mt: 4 }}>
+      <Box sx={{ mt: 4, height: '300px' }}>
         I am the parent and handle scroll events
         <GlowingOnRenderBox sx={{ height: '120px' }}>I am an expensive component</GlowingOnRenderBox>
       </Box>
